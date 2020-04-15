28b1d4a183946ddabc61cc08b3eb3bac901b9516
angiegyi <angelagyi@hotmail.com>
2020-04-15 13:53:20 +0930

    made it O(n)

--- Assignment_2/Task_1.py
+++ Assignment_2/Task_1.py
@@ -7,8 +7,12 @@ def longest_oscillation_greedy(L):
     if len(L) == 0:
         return [0]
 
+    if len(L) == 1:
+        return [1]
+
+
     #initial check
-    flag = determine_flag(L[0],L[1])
+    needs_max = determine_flag(L[0],L[1])
 
     i = 0
     j = i + 1
@@ -17,34 +21,41 @@ def longest_oscillation_greedy(L):
         prev_j = j
 
         # iterate through: looking for the next biggest element
-        if flag is True:
+        if needs_max is True:
             if L[i] < L[j]: #if previous < next
-                while j < len(L)-1 and L[j+1] > L[j]: #is finding the peak
+                while j < len(L)-1 and L[j+1] > L[j]: #is finding the max elemen
                     memo[j] = memo[i] #while it hasnt found the peak, the best we can do is the previous one
                     j += 1
-                memo[j] = max(memo[i]+1, (memo[j]))
-                j = prev_j
-                flag = False
+                if j == len(L)-1:  #to stop it from going n^2
+                    memo[j] = max(memo[i] + 1, (memo[j]))
+                    break
+                else:
+                    memo[j] = max(memo[i]+1, (memo[j]))
+                    j = prev_j
+                    needs_max = False
 
         # looking for the next smallest element
-        elif flag is False:
+        elif needs_max is False:
             if L[i] > L[j]: #if previous > next
-                while j < len(L) - 1 and L[j + 1] < L[j]: #
+                while j < len(L) - 1 and L[j + 1] < L[j]: #iterates through to find
                     memo[j] = memo[i]
                     j += 1
-                memo[j] = max(memo[i]+1, (memo[j]))
-                flag = True
-                j = prev_j
+                if j == len(L)-1: #to stop it from going n^2
+                    memo[j] = max(memo[i] + 1, (memo[j]))
+                    break
+                else:
+                    memo[j] = max(memo[i]+1, (memo[j]))
+                    needs_max = True
+                    j = prev_j
 
         #if two items are equal
         else:
             if j > len(L)-2:
                 j += 1
                 i += 1
-
             else:
                 memo[j] = memo[i]
-                flag = determine_flag(L[i],L[j+1])
+                needs_max = determine_flag(L[i],L[j+1])
 
         j += 1
         i += 1
@@ -80,7 +91,7 @@ def get_numbers(memo):
     for i in range(len(memo)):
         current = memo[i]
         if (i != len(memo)-1):
-            if memo[i+1]== current:
+            if memo[i + 1] <= current:
                 continue
         output_index.append(i)
 
@@ -90,4 +101,4 @@ def get_numbers(memo):
 
     return output_index
 
-print(longest_oscillation_greedy([3,3,3,3,3,3]))
+print(longest_oscillation_greedy([0]))
