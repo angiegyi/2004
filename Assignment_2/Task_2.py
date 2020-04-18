c6a0e12c340810fd6e3319552b48db92147eaf0d
angiegyi <angelagyi@hotmail.com>
2020-04-18 17:52:13 +0930

    added valid neighbour

--- Assignment_2/Task_2.py
+++ Assignment_2/Task_2.py
@@ -5,8 +5,6 @@ def valid_neighbour(matrix, current_position):
     :param current_position:
     :return:
     """
-
-
     direction = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 
     original_row = current_position[1]
@@ -17,19 +15,40 @@ def valid_neighbour(matrix, current_position):
     for i in range(len(direction)):
         row_dir = direction[i][0]
         col_dir = direction[i][1]
-        if (original_row + row_dir >= 0 and original_row + row_dir < len(matrix)) and (original_column+ col_dir >= 0 and original_column + col_dir < len(matrix[0])):
+        if (0 <= original_row + row_dir < len(matrix)) and (0 <= original_column+ col_dir < len(matrix[0])):
+            #if the original position < desired position
             if matrix[original_row][original_column] < matrix[row_dir+original_row][col_dir+original_column]:
                 valid_neighbour.append((original_row+row_dir,original_column+col_dir))
+
 
     return valid_neighbour
 
 
-a = [
-    [3,4,6,7],
-    [5,8,3,1]
+def longest_walk(M):
 
-    ]
+    if len(M) == 0:
+        return 0
 
-# 0 = row (y) 1 = col (x)
-print(len(a[0])/len(a))
-print(valid_neighbour(a,(0,0)))
+    memo = [[0 for _ in range(len(M[0]))] for _ in range(len(M))]
+
+    for i in range(len(M)):
+        for j in range(len(M[0])):
+            if memo[i][j] == 0:
+                memo[i][j] = calculate_walk(M,i,j)
+            else:
+                memo[i][j] += 1
+    return memo[-1][-1]
+
+
+
+def calculate_walk(matrix, i ,j):
+
+    valid = valid_neighbour(matrix,(i,j))
+    max_length = 0
+
+    for move in valid:
+        path_length = 1 + calculate_walk(matrix,move[0],move[1])
+        max_length = max(max_length,path_length)
+    return max_length
+
+print(longest_walk([[4,6],[7,2]]))
